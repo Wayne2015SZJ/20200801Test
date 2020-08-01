7b186ac431a3e11608cd3afb87da1395999d4318
SZJ <mac@SZJ-2.local>
2020-08-01 19:17:41 +0800

    hah

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-        System.out.println("branch1");
-=======
         System.out.println("branch2");
->>>>>>> branch2
 	}
 }
